4ab15845e50bd433227309fb30138f0570f8067a
Martin Klissarov <martin.klissarov@hotmail.com>
2017-12-15 14:22:39 +0100

    Delete tf_util_supp.py

--- tf_util_supp.py
+++
@@ -1,9 +0,0 @@
-def dense3D2(x, size, name, option, num_options=1, weight_init=None, bias=True):
-    w = tf.get_variable(name + "/w", [num_options, x.get_shape()[1], size], initializer=weight_init)
-    ret = tf.matmul(x, w[option[0]])
-    if bias:
-        b = tf.get_variable(name + "/b", [num_options,size], initializer=tf.zeros_initializer())
-        return ret + b[option[0]]
-
-    else:
-        return ret
